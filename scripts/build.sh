f577ef7b234c5f2248e6245066f2c3b42b470e13
Justin Albano <albano.justin@gmail.com>
2024-07-14 15:26:41 -0400

    Added GitHub workflow.

---
+++ scripts/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+PROJECT_ROOT=$SCRIPT_DIR/..
+
+mvn clean package -f ${PROJECT_ROOT}/libraries/pom.xml
---
+++ scripts/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+PROJECT_ROOT=$SCRIPT_DIR/..
+
+mvn test -f ${PROJECT_ROOT}/libraries/pom.xml
