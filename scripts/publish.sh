f1f2d2148d61c553f05adc00db1077dd5cb025f2
Justin Albano <albano.justin@gmail.com>
2024-07-15 17:40:54 -0400

    Established GPG ring and removed example project.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,4 +3,10 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT=$SCRIPT_DIR/..
 
-mvn deploy -f ${PROJECT_ROOT}/libraries/pom.xml $@
+mvn -f ${PROJECT_ROOT}/libraries/pom.xml \
+	release:prepare \
+	release:perform \
+	javadoc:jar \
+	source:jar \
+	deploy \
+	$@
