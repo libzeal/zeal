b10970f69cb8aeb66a2cdb78f0ef4e435bb709ed
Justin Albano <albano.justin@gmail.com>
2024-07-16 18:23:36 -0400

    Improved automated release message (added missing code from last merge).

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT=$SCRIPT_DIR/..
 
-cd "${PROJECT_ROOT}" || exit 1
+cd "$PROJECT_ROOT" || exit 1
 
 mvn release:prepare \
     release:perform \
