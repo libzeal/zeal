0050d33d7ebe4b2e9f46d891c28d30f7c26f6315
Justin Albano <albanoj2@users.noreply.github.com>
2024-07-16 10:12:43 -0400

    Create sonarqube.sh

---
+++ scripts/sonarqube.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+PROJECT_ROOT=$SCRIPT_DIR/..
+
+mvn -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+  -Dsonar.projectKey=libzeal_zeal \
+  -f ${RPOJECT_ROOT}/libraries/pom.xml \
+  $@
