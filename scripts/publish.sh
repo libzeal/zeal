0dbb6704dca9560d6679c4c73af69e4976d248d8
Justin Albano <albano.justin@gmail.com>
2024-07-16 01:27:44 -0400

    Added snapshot back in.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT=$SCRIPT_DIR/..
 
 mvn -X -f ${PROJECT_ROOT}/libraries/pom.xml \
-    release:prepare -DtagBase=https://github.com/libzeal/zeal/tags \
+    release:prepare \
     release:perform \
-	deploy \
 	$@
