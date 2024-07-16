3bbe13ccccf2fd4958d280720ea3c1a67d53f2ea
Justin Albano <albanoj2@users.noreply.github.com>
2024-07-16 10:39:44 -0400

    Replaced with sonar profile

--- scripts/sonarcloud.sh
+++ scripts/sonarcloud.sh
@@ -3,7 +3,6 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT=$SCRIPT_DIR/..
 
-mvn -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
-  -Dsonar.projectKey=libzeal_zeal \
+mvn -B verify -Psonar \
   -f ${PROJECT_ROOT}/libraries/pom.xml \
   $@
