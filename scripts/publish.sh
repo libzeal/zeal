b09f9e60775786ccce7191bb1f5901ad0a1c791f
Justin Albano <albano.justin@gmail.com>
2024-07-16 16:36:01 -0400

    Reorganized project to see if that fixes the Maven release bug.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT=$SCRIPT_DIR/..
 
-mvn -X -f ${PROJECT_ROOT}/libraries/pom.xml \
+mvn -X -f ${PROJECT_ROOT}/pom.xml \
     release:prepare \
     release:perform \
 	$@
