6878638a891e2cd3efb230d79748d0459e51d728
Justin Albano <albano.justin@gmail.com>
2024-07-15 22:31:51 -0400

    Removed publish POM file path.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,8 +3,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT=$SCRIPT_DIR/..
 
-mvn -f ${PROJECT_ROOT}/libraries/pom.xml \
-	release:prepare \
+mvn release:prepare \
 	release:perform \
 	javadoc:jar \
 	source:jar \
