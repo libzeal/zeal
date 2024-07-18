48598ae108c73079b3470dcd30dae56a8a14c635
Justin Albano <albanoj2@users.noreply.github.com>
2024-07-17 23:24:44 -0400

    Added foundation for tests and added minimal JavaDocs

--- zeal-expression/src/main/java/io/github/libzeal/zeal/expression/Expression.java
+++ zeal-expression/src/main/java/io/github/libzeal/zeal/expression/Expression.java
@@ -2,7 +2,18 @@ package io.github.libzeal.zeal.expression;
 
 import io.github.libzeal.zeal.expression.evalulation.EvaluatedExpression;
 
+/**
+ * An expression that can be evaluated.
+ *
+ * @author Justin Albano <albano.justin@gmail.com>
+ * @since 0.2.0
+ */
 public interface Expression {
 
+    /**
+     * Evaluates the expression.
+     *
+     * @return The evaluated expression.
+     */
     EvaluatedExpression evaluate();
 }
