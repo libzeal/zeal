fbec85164e2ef81ddcbc441fab0a9b86de3139c9
Justin Albano <albano.justin@gmail.com>
2024-09-01 20:18:53 -0400

    Added documentation for cause classes (#10).

--- zeal-logic/src/main/java/io/github/libzeal/zeal/logic/evaluation/cause/CauseGenerator.java
+++ zeal-logic/src/main/java/io/github/libzeal/zeal/logic/evaluation/cause/CauseGenerator.java
@@ -2,15 +2,55 @@ package io.github.libzeal.zeal.logic.evaluation.cause;
 
 import io.github.libzeal.zeal.logic.evaluation.Evaluation;
 
+<<<<<<< HEAD
 @FunctionalInterface
 public interface CauseGenerator {
 
     Cause generate(Evaluation evaluation);
 
+=======
+/**
+ * A generator used to defer the creation of {@link Cause} instances. This is used since the cause of an evaluation may
+ * refer to the evaluation (a circular creation if deferral is not available).
+ *
+ * @author Justin Albano
+ * @since 0.2.1
+ */
+@FunctionalInterface
+public interface CauseGenerator {
+
+    /**
+     * Creates a new cause with the supplied evaluation at the cause evaluation.
+     *
+     * @param evaluation
+     *     The evaluation to create a cause for.
+     *
+     * @return A new cause.
+     */
+    Cause generate(Evaluation evaluation);
+
+    /**
+     * A generator that will create causes that refer to the evaluation only (no underlying cause).
+     *
+     * @return A new cause.
+     */
+>>>>>>> c66d1f7 (Added documentation for cause classes (#10).)
     static CauseGenerator self() {
         return Cause::new;
     }
 
+<<<<<<< HEAD
+=======
+    /**
+     * A generator that will create causes that refer to the evaluation with an underlying cause matching the supplied
+     * cause.
+     *
+     * @param cause
+     *     The underlying cause.
+     *
+     * @return A new cause.
+     */
+>>>>>>> c66d1f7 (Added documentation for cause classes (#10).)
     static CauseGenerator withUnderlyingCause(final Cause cause) {
         return evaluation -> new Cause(evaluation, cause);
     }
--- zeal-logic/src/main/java/io/github/libzeal/zeal/logic/evaluation/cause/MaximumDepthExceededException.java
+++ zeal-logic/src/main/java/io/github/libzeal/zeal/logic/evaluation/cause/MaximumDepthExceededException.java
@@ -1,4 +1,24 @@
 package io.github.libzeal.zeal.logic.evaluation.cause;
 
+<<<<<<< HEAD
 public class MaximumDepthExceededException extends RuntimeException{
+=======
+/**
+ * An exception denoting that the maximum depth of the chain has been exceeded.
+ *
+ * @author Justin Albano
+ * @since 0.2.1
+ */
+public class MaximumDepthExceededException extends RuntimeException {
+
+    /**
+     * Creates a new exception with the supplied maximum depth.
+     *
+     * @param maximumDepth
+     *     The maximum depth that was exceeded.
+     */
+    public MaximumDepthExceededException(final int maximumDepth) {
+        super("Maximum depth exceeded: " + maximumDepth);
+    }
+>>>>>>> c66d1f7 (Added documentation for cause classes (#10).)
 }
